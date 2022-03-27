d593eeccf1bc81c1724ce07b92cc528fc9c1e0bc
czaree <87497146+czaree@users.noreply.github.com>
2022-03-27 02:28:12 +0000

    Initial commit

---
+++ local/script.js
@@ -0,0 +1,15 @@
+const menuBtn =
+  document.querySelector(".menu-button");
+
+let showMenu = false;
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  /*if (!showMenu) {
+    menuBtn.classlist.add("close");
+    showMenu = true;
+  } else {
+    menuBtn.classlist.remove("close");
+    showMenu = false;
+  }*/
+}
